4ecb184421117a1b69d6ed64edceefd66b230cf7
DayoungChoii <young12430@gmail.com>
2023-05-08 22:23:24 +0900

    [kotlin example] 책 통계 보여주기

---
+++ src/main/java/com/group/libraryapp/dto/book/response/BookStatResponse.kt
@@ -0,0 +1,10 @@
+package com.group.libraryapp.dto.book.response
+
+import com.group.libraryapp.domain.book.BookType
+
+data class BookStatResponse (
+    var count: Int,
+    val type: BookType,
+){
+    fun plusOne() = this.count++
+}
--- src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
+++ src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
@@ -1,15 +1,31 @@
 package com.group.libraryapp.dto.user.response
 
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
+
 data class UserLoanHistoryResponse(
     val name: String,
     val books: List<BookHistoryResponse>
 ) {
+    companion object {
+        fun from(user: User): UserLoanHistoryResponse =
+            UserLoanHistoryResponse(
+                name = user.name,
+                books = user.userLoanHistories.map(BookHistoryResponse::of)
+            )
 
+    }
 }
 
 data class BookHistoryResponse(
     val name: String,
     val isReturn: Boolean,
 ) {
-
+    companion object {
+        fun of(history: UserLoanHistory): BookHistoryResponse {
+            return BookHistoryResponse(name = history.bookName,
+                isReturn = history.isReturn)
+        }
+    }
 }
