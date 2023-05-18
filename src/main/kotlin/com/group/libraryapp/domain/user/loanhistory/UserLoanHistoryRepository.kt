2aa5ad6ee2099cd267305ea38ed25a8fdb7a05f6
DayoungChoii <young12430@gmail.com>
2023-05-19 00:23:25 +0900

    [kotlin example] querydsl 적용3

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -3,10 +3,4 @@ package com.group.libraryapp.domain.user.loanhistory
 import com.group.libraryapp.domain.book.BookType
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
-    fun findByBookNameAndStatus(bookName: String, userLoanStatus: UserLoanStatus): UserLoanHistory?
-
-    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
-
-    fun countByStatus(status: UserLoanStatus): Long
-}
+interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long>
